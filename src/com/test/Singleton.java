86d8c2655a5585c7e84315df7fa2b6384ff3d8f1
SOGR <SOGR@192.168.0.6>
2022-06-28 19:21:52 +0530

    first commit

---
+++ src/com/test/Singleton.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class Singleton {
+	private static Singleton obj=new Singleton();//Early, instance will be created at load time
+	
+	private Singleton() {
+		
+	}
+	public void doSomething(){  
+		 //write your code  
+		 }  
+}
